38bedf37020e37709ab953a1d2b07556fabff256
Felipe Montoya <felipe.montoya@edunext.co>
2019-08-30 15:58:40 -0500

    Adding the basic info view

---
+++ eox_theming/management/urls.py
@@ -0,0 +1,11 @@
+"""
+eox_theming management urls
+"""
+from django.conf.urls import url
+
+from eox_theming.management import views
+
+
+urlpatterns = [
+    url(r'^eox-info$', views.info_view),
+]
---
+++ eox_theming/management/views.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""The views for the plugin management"""
+
+from __future__ import unicode_literals
+
+import json
+from os.path import dirname, realpath
+from subprocess import check_output, CalledProcessError
+
+from django.http import HttpResponse
+from django.utils import six
+
+
+import eox_theming
+
+
+def info_view(request):
+    """
+    Basic view to show the working version and the exact git commit of the
+    installed app
+    """
+    try:
+        working_dir = dirname(realpath(__file__))
+        git_data = six.text_type(check_output(
+            ["git", "rev-parse", "HEAD"], cwd=working_dir))
+    except CalledProcessError:
+        git_data = ""
+
+    response_data = {
+        "version": eox_theming.__version__,
+        "name": "eox-theming",
+        "git": git_data.rstrip('\r\n'),
+    }
+    return HttpResponse(
+        json.dumps(response_data),
+        content_type="application/json"
+    )
