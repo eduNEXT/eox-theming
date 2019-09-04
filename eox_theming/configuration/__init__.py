71dd693db879e719733aae6d763864d8c161034e
Felipe Montoya <felipe.montoya@edunext.co>
2019-09-04 18:23:31 -0500

    Actually reading the options from a list of sources

--- eox_theming/configuration/__init__.py
+++ eox_theming/configuration/__init__.py
@@ -1,14 +1,31 @@
 """
 Manages the diferent access, storage locations and methods used to read the configuration of a theme
 """
+from django.conf import settings
+from eox_theming.edxapp_wrapper import config_sources
 
 
 class ThemingConfiguration(object):
     """
     This class must handle all the calls to the diferential settings a theme will be allowed to use
     """
+    source_functions = [getattr(config_sources, name) for name in settings.EOX_THEMING_CONFIG_SOURCES]
 
-    @staticmethod
-    def options(*args, **kwargs):  # pylint: disable=unused-argument
+    @classmethod
+    def options(cls, *args, **kwargs):  # pylint: disable=unused-argument
         """Main method for accessing the current configuration for the theme"""
-        return 'example'
+        value = None
+
+        for source in cls.source_functions:
+            try:
+                value = source(*args)
+                if value:
+                    break
+            except Exception:
+                # LOG
+                continue
+
+        if not value:
+            value = kwargs.pop('default', None)
+
+        return value
