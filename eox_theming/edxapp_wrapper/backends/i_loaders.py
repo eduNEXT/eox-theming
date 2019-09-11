528f92d088e6cd2f162c4258bd8fbbd87b061ae5
Luis Moreno <luis.moreno@edunext.co>
2019-09-11 18:56:36 -0400

    Extend ThemeTemplateLoader

--- eox_theming/edxapp_wrapper/backends/i_loaders.py
+++ eox_theming/edxapp_wrapper/backends/i_loaders.py
@@ -1,9 +1,14 @@
 """
 Simple backend that returns the platform's ThemeTemplateLoader class
 """
-from openedx.core.djangoapps.theming.template_loaders import ThemeTemplateLoader  # pylint: disable=import-error
+from openedx.core.djangoapps.theming.template_loaders import ThemeTemplateLoader, ThemeFilesystemLoader  # pylint: disable=import-error
 
 
 def get_openedx_theme_loader():
     """Return the template loader class when called during runtime"""
     return ThemeTemplateLoader
+
+
+def get_theme_filesystem_loader():
+    """Return the filesystem loader class when called during runtime"""
+    return ThemeFilesystemLoader
--- eox_theming/edxapp_wrapper/loaders.py
+++ eox_theming/edxapp_wrapper/loaders.py
@@ -7,7 +7,11 @@ from django.conf import settings
 
 def get_openedx_theme_loader():
     """ Get the Open edX template loader class. """
-
     backend = import_module(settings.EOX_THEMING_BASE_LOADER_BACKEND)
-
     return backend.get_openedx_theme_loader()
+
+
+def get_theme_filesystem_loader():
+    """ Get the Open edX filesystem loader class. """
+    backend = import_module(settings.EOX_THEMING_BASE_LOADER_BACKEND)
+    return backend.get_theme_filesystem_loader()
