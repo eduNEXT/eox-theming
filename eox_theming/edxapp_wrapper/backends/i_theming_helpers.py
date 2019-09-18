db604647fb67920baceede73c774ae40bc067151
morenol <13-10934@usb.ve>
2019-09-18 14:54:32 -0400

    Revert "Method to get the parent theme"

--- eox_theming/edxapp_wrapper/backends/i_theming_helpers.py
+++
@@ -1,7 +0,0 @@
-""" Backend abstraction for theming helpers. """
-from openedx.core.djangoapps.theming import helpers as theming_helpers  # pylint: disable=import-error
-
-
-def get_theming_helpers():
-    """ Backend to get the theming helpers. """
-    return theming_helpers
--- eox_theming/edxapp_wrapper/backends/i_theming_helpers_tests.py
+++
@@ -1,6 +0,0 @@
-""" Backend abstraction for theming helpers tests. """
-
-
-def get_theming_helpers():
-    """ Backend abstraction used on the tests """
-    return object
