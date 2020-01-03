bfc966c3cc282bb4ea75c090f2b50ff6e0d8b2bf
Felipe Montoya <felipe.montoya@edunext.co>
2020-01-02 21:34:00 -0500

    Making the test read the cases from the text files

--- eox_theming/tests/api_v1/test_use_cases.py
+++ eox_theming/tests/api_v1/test_use_cases.py
@@ -2,40 +2,64 @@
 """
 Tests for the custom template tags
 """
-from django.test import TestCase
-from django.template import Context, Template
-from parameterized import parameterized
-from mock import patch
+import glob, os
 import json
+
+from django.test import TestCase
+from django.test.utils import override_settings
+from parameterized import parameterized
+
+from eox_theming import configuration
 
 
 def get_all_test_cases():
-    return [
-        'case_001.txt',
-    ]
+    """
+    Finds all the files in the same directory as this that end in .txt
+    """
+    results = []
+    for pathname in glob.glob('{}/*.txt'.format(os.path.dirname(os.path.abspath(__file__)))):
+        name = pathname.split('/')[-1].split('.')[0]
+        results.append((name, pathname))
+
+    return results
 
 
 class TestsUseCasesSequence(TestCase):
+    """
+    Execute all the tests cases in a dynamic test definition
+    """
 
     def run_case(self, data, code, output):
+        """
+        Run the case as defined in the text file
+        """
         clean_data = ''.join(data.splitlines())
         case_data = json.loads(clean_data)
 
-        case_result = eval(code)
+        my_globals = {
+            'theming': configuration.ThemingConfiguration()
+        }
 
-        # print('======')
-        # print(case_result)
-        # print('======')
-        # print(''.join(output.splitlines()))  # si es esto?
-        # print('======')
+        with override_settings(THEME_OPTIONS=case_data):
+            case_result = eval(code, my_globals)
 
+        # For now we compare the results as being the same without
+        # trailing and leading spaces
         self.assertEqual(case_result, ''.join(output.splitlines()))
 
 
     @parameterized.expand(get_all_test_cases)
-    def test_case(self, name):
+    def test_case(self, name, pathname):
+        """
+        Dynamic test definition to run every case in this directory.
 
-        f = open('eox_theming/tests/api_v1/{}'.format(name), 'r')
+        The tests definitions must be files with the .txt extension,
+        which must have 3 sections, one for the data to be loaded in
+        the configuration, one for the lines that exemplify the code
+        execution and one for the html or string results.
+        """
+
+        f = open(pathname, 'r')
         text = f.read()
         blocks = text.split("======")
 
@@ -46,5 +70,3 @@ class TestsUseCasesSequence(TestCase):
                 test_case[key] = blocks[idx + 1]
 
         self.run_case(**test_case)
-
-        # self.fail()
