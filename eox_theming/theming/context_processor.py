1bc6ea02335bd1676d0d70b3ba4b8d820e8ab47f
jfavellar90 <jhony.avella@edunext.co>
2020-01-15 16:27:22 -0500

    Changing the Theme API to the one exposed in the V1

--- eox_theming/theming/context_processor.py
+++ eox_theming/theming/context_processor.py
@@ -3,7 +3,9 @@ This context processor is added to every request to render a mako or django temp
 
 It can be used using the theming.options(args) helper.
 """
+from eox_theming.edxapp_wrapper.configuration_helpers import get_configuration_helper
 from eox_theming.configuration import ThemingConfiguration
+from eox_theming.api.v1.api import ThemingOptions
 
 
 def eox_configuration(request):  # pylint: disable=unused-argument
@@ -11,6 +13,21 @@ def eox_configuration(request):  # pylint: disable=unused-argument
     It inserts a theming object with the options helper function
     This is the last processor to be run.
     """
+    theming_api = get_active_theming_api()
     return {
-        'theming': ThemingConfiguration,
+        'theming': theming_api,
     }
+
+
+def get_active_theming_api():
+    """
+    Determine which Theming API to use in the frontend
+    """
+    configuration_helpers = get_configuration_helper()
+    localizer = {
+        "V0": ThemingConfiguration,
+        "V1": ThemingOptions()
+    }
+
+    current_api = configuration_helpers.get_value('EOX_THEMING_CURRENT_THEMING_API', 'V1')
+    return localizer.get(current_api, localizer['V1'])
