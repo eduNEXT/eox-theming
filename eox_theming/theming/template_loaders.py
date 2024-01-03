b78c0b872e4c541c3e1b637af1c0088fa674552b
Ever3tt <jsebaspj10@gmail.com>
2024-01-03 10:27:15 -0500

    fix: improved handling of empty directories in get_dirs method

--- eox_theming/theming/template_loaders.py
+++ eox_theming/theming/template_loaders.py
@@ -93,4 +93,6 @@ class EoxThemeFilesystemLoader(ThemeFilesystemLoader):
         https://github.com/eduNEXT/edunext-platform/blob/master/openedx/core/djangoapps/theming/template_loaders.py#L39
         instead of using the cached values of self.dirs on runtime.
         """
-        return ThemeFilesystemLoader.get_theme_template_sources()
+        theme_dirs = ThemeFilesystemLoader.get_theme_template_sources()
+
+        return theme_dirs if theme_dirs else super().get_dirs()
