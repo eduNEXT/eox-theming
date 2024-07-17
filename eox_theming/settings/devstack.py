dd2a5fa606aefaef3a7849c63f7929144754064a
Diana Catalina Olarte <diana.olarte@edunext.co>
2024-07-18 01:07:01 +1000

    docs: fix links reference to opnedx scope

--- eox_theming/settings/devstack.py
+++ eox_theming/settings/devstack.py
@@ -8,7 +8,7 @@ from __future__ import unicode_literals
 def plugin_settings(settings):
     """
     Set of plugin settings used by the Open Edx platform.
-    More info: https://github.com/edx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
+    More info: https://github.com/openedx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
     """
     settings.STATICFILES_FINDERS = [
         'eox_theming.theming.finders.EoxThemeFilesFinder',
