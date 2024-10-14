5d96aa8e9dc531612631823a19d884b267c2e7b6
magajh <maria.jaimes@edunext.co>
2024-10-14 15:10:36 -0400

    fix: leave script to execute integration tests empty

--- scripts/execute_integration_tests.sh
+++ scripts/execute_integration_tests.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-echo "Install requirements"
-make install-dev-dependencies
-
-echo "Run tests"
-# Since we don't have any integration tests at the moment,
-# we allow pytest to pass without running any tests.
-pytest -rPf --allow-no-tests
-
+# As we currently don't have any integration tests,
+# this script is intentionally left empty.
